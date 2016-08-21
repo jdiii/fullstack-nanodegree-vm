3479397672573f2fee4c30ea39f05e24c807644c
johndavey <john.n.davey@gmail.com>
2016-08-21 09:45:33 -0400

    Add table defs to tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+\c tournament;
+
+CREATE TABLE players(
+	name text NOT NULL,
+	id serial PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE matches(
+	id serial PRIMARY KEY NOT NULL,
+	winner int REFERENCES players (id) CHECK (winner != loser) NOT NULL,
+	loser int REFERENCES players (id)
+);
